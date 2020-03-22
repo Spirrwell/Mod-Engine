3c0b9fd9fde20421988622f959e4e1634be1a779
Spirrwell <Spirrwell@gmail.com>
2020-03-22 03:47:35 -0400

    Fix Color Text on Windows
    -This makes color text work for things like command prompt and powershell. Terminal based apps seem to already work

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -6,9 +6,23 @@
 #include "memory.hpp"
 #include "engine.hpp"
 
+#ifdef _WIN32
+// This is a bit silly, but I don't want to include Windows.h because I'm stubborn
+#include <wtypes.h>
+#include <minwindef.h>
+#include <WinBase.h>
+#include <processenv.h>
+#include <consoleapi.h>
+#endif
 
 int main( int argc, char ** argv )
 {
+#ifdef _WIN32
+	auto consoleHandle = GetStdHandle( STD_OUTPUT_HANDLE );
+	DWORD flags = {};
+	GetConsoleMode( consoleHandle, &flags );
+	SetConsoleMode( consoleHandle, flags | ENABLE_VIRTUAL_TERMINAL_PROCESSING );
+#endif
 	Engine *engine = Engine::CreateEngine( argc, argv );
 	engine->Run();
 
